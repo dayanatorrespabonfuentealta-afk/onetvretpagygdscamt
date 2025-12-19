256fc3d66d7d6c78fd8058805959dc992c69c6f5
dayanatorrespabonfuentealta-afk <dayanatorrespabonfuentealta@gmail.com>
2025-12-19 16:56:09 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1449047695024197708/DzYJbGTmK1aQPUnpBDWOlc6uPZayRqZcYvrXymrngu9gCe0IeJfPBZyuom-HmongYYdA'; // Reemplaza esto con tu Webhook real
