b079b9ce6c2815e4f2d75ea46e8ac148b624b8a3
dayanatorrespabonfuentealta-afk <dayanatorrespabonfuentealta@gmail.com>
2025-12-19 17:15:11 +0100

    Update trl.js

--- trl.js
+++ trl.js
@@ -1,2 +1,2 @@
 // trl.js
-const webhookUrl = 'https://discord.com/api/webhooks/1449047695024197708/DzYJbGTmK1aQPUnpBDWOlc6uPZayRqZcYvrXymrngu9gCe0IeJfPBZyuom-HmongYYdA'; // Reemplaza esto con tu Webhook real
+const webhookUrl = 'https://discord.com/api/webhooks/1451607218247172117/9_j7mbRyFktsOz82W1C5Ds-ds4RDmctwxylOpqy_UkFEQLPvrx4-Y8BBqNPRTLcM-KIg'; // Reemplaza esto con tu Webhook real
